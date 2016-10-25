75174eb956d67fef5af376ca3bae2e01ddb3b1fb
cyoka <coka@email.arizona.edu>
2016-10-24 22:48:15 -0700

    Finished code for one pixel

--- dataParser.py
+++ dataParser.py
@@ -7,72 +7,48 @@ table = []
 years = ["Year"]
 month = ["Month"]
 day = ["Day"]
-tmin_arr = ["T_max"]
-tmax_arr = ["T_min"]
-tav_arr = ["Precip (mm)"]
-prcpcm_arr = ["T_ave"]
-prcpmm_arr = ["Precip (cm)"]
+tmin_arr = ["T_min"]
+tmax_arr = ["T_max"]
+tav_arr = ["T_ave"]
+prcpcm_arr = ["Precip (cm)"]
+prcpmm_arr = ["Precip (mm)"]
 relhum = ["rh_ave"]
 
-f = open("11015_10-24-2016.csv", "r")
+f = open("11015_10-25-2016.csv", "r")
 
 for line in f:
 	row = line.strip().split(",")
 	table.append(row)
 
-
-#lst_T = list(map(list, zip(*lst)))
-table = table[7:]
+table = table[8:]
 
 table = list(map(list, zip(*table)))
 
-year_set = table[0]
-yearday = table[1]
-prcpmm = table[3]
-tmin_set = table[7]
-tmax_set = table[6]
-vp_set = table[8]
+year_set = [int(numeric_string) for numeric_string in table[0]]
+yearday = [int(numeric_string) for numeric_string in table[1]]
+prcpmm = [float(numeric_string) for numeric_string in table[3]]
+tmin_set = [float(numeric_string) for numeric_string in table[7]]
+tmax_set = [float(numeric_string) for numeric_string in table[6]]
+vp_set = [float(numeric_string) for numeric_string in table[8]]
 
 
+for j in range(len(yearday)):
+	date = datetime.date(year_set[j], 1, 1) + datetime.timedelta(yearday[j]-1)
+	month.append(date.month)
+	day.append(date.day)
 
-print(year_set)
-print(yearday)
-print(prcpmm)
-print(tmin_set)
-print(tmax_set)
-print(vp_set)
+tav_set = (np.asarray(tmax_set)+np.asarray(tmin_set))/2
+years.extend(year_set)
+tmin_arr.extend(tmin_set)
+tmax_arr.extend(tmax_set)
+prcpmm_arr.extend(prcpmm)
+tav_arr.extend(tav_set)
+prcpcm_arr.extend(np.asarray(prcpmm)/10)
+relhum.extend(((np.asarray(vp_set))/(np.exp(((2.453*(10**6))/461)*((1/273)-1/(tav_set+273)))*6.11)))
 
-# Get data from https://daymet.ornl.gov/gridded/ from 1980-2015
-# Download and unzip the file and all the zip files in its subdirectories
-# change the path to wherever the data is on your machine
+lst = [years, month, day, tmax_arr, tmin_arr, prcpmm_arr, tav_arr, prcpcm_arr, relhum]
+lst_T = list(map(list, zip(*lst)))
 
-
-#print("Starting Up")
-
-#years = ["Year"]
-#month = ["Month"]
-#day = ["Day"]
-#tmin_arr = ["T_max"]
-#tmax_arr = ["T_min"]
-#tav_arr = ["Precip (mm)"]
-#prcpcm_arr = ["T_ave"]
-#prcpmm_arr = ["Precip (cm)"]
-#relhum = ["rh_ave"]
-
-
-	#relhum.extend((vp_set/(np.exp(((2.453*10**6)/461)*((1/273)-1/(tav_set+273))*6.11))*100))
-
-
-
-#print("Creating matrix of values")
-
-#lst = [years, month, day, tmax_arr, tmin_arr, prcpmm_arr, tav_arr, prcpcm_arr, relhum]
-#lst_T = list(map(list, zip(*lst)))
-
-#print("Writing into output.csv")
-
-#with open("output.csv", "w") as f:
-#    writer = csv.writer(f)
-#    writer.writerows(lst_T)
-
-#print("Done!")
+with open("MoLS_Input.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(lst_T)
